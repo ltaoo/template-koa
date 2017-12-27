7fdba816a108a8bfc83de603bdd90162cfbef3c4
ltaoo <litaowork@aliyun.com>
2017-12-27 15:27:51 +0800

    feat: 首页渲染请求接口得到的数据

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,9 @@
-let index = (ctx, next) => {
-	ctx.render('index.html', {title: 'Index Page'})
+const fetch = require('isomorphic-fetch');
+
+let index = async (ctx, next) => {
+    const res = await fetch('https://easy-mock.com/mock/5a1d30028e6ddb24964c2d91/business/api/accnt');
+    const data = await res.json();
+    ctx.render('index.html', {title: 'Index Page', data: JSON.stringify(data)});
 }
 
 module.exports = {
